28fa6940409b1e55022f05bc87b55fddc87c8a2c
katsuhisa yuasa <berupon@gmail.com>
2021-11-16 04:37:16 +0900

    DrawFilledEllipse 関数のアンチエイリアシング度合いを調整

--- graphics/graphics_DrawFilledEllipse.cpp
+++ graphics/graphics_DrawFilledEllipse.cpp
@@ -9,29 +9,53 @@
 
 namespace Graphics {
 
-void DrawFilledEllipse(float cx, float cy, float width, float height, pixel_t color)
-{
-  float a = width;
-  float b = height;
-  float aa = a * a;
-  float bb = b * b;
-
-  float rcp = 1.0f / (bb * aa);
-  for (float y = -height; y <= height; y++) {
-    float yy = y * y;
-    float yyaa = yy * aa;
-    for (float x = -width; x <= width; x++) {
-      float xx = x * x;
-      float dist = (1.0f - (xx * bb + yyaa) * rcp) * std::min(width, height) * 2.0f;
-      if (dist > 1.0f)
-        PutPixel(cx + x, cy + y, color);
-      else if (dist < 0.0f)
-        dist = 0.0f;
-      else
-        PutPixel(cx + x, cy + y, AdjustAlpha(color, dist));
-    }
-  }
-}
+	void DrawFilledEllipse(float cx, float cy, float width, float height, pixel_t color)
+	{
+		if (width == 0 || height == 0) {
+			return;
+		}
+		float halfWidth = width / 2.0f;
+		float halfHeight = height / 2.0f;
+		float w = halfWidth;
+		float h = halfHeight;
+		float ww = w * w;
+		float hh = h * h;
+		float wwhh = ww * hh;
+		float whmin = std::min(halfWidth, halfHeight);
+		for (float y = -halfHeight; y <= +halfHeight; y++) {
+			float yy = y * y;
+			float yyww = yy * ww;
+			float sharpness = 0.5f;
+			float ratio = abs(y) / halfHeight;
+			if (width > height) {
+				ratio = 1.0f - ratio;
+				ratio *= ratio;
+				ratio *= ratio;
+				ratio *= 3.0f;
+				sharpness = 0.5 + ratio;
+			}
+			for (float x = -halfWidth; x <= +halfWidth; x++) {
+				if (height > width) {
+					ratio = abs(x) / halfWidth;
+					ratio = 1.0f - ratio;
+					ratio *= ratio;
+					ratio *= ratio;
+					ratio *= 3.0f;
+					sharpness = 0.5 + ratio;
+				}
+				float xx = x * x;
+				float xxhh = xx * hh;
+				float dist = (1.0f - (xxhh + yyww) / wwhh) * whmin * sharpness;
+				if (dist >= 1.0f) {
+					PutPixel(cx + x, cy + y, color);
+				}else if (dist <= 0.0f) {
+					dist = 0.0f;
+				}else {
+					PutPixel(cx + x, cy + y, AdjustAlpha(color, dist));
+				}
+			}
+		}
+	}
 
 } // namespace Graphics {
 
